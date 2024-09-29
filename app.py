513304c6da1ec3c4c39a696b5f6c8357ef4d8512
standardmountain <140400957+standardmountain@users.noreply.github.com>
2024-09-29 11:18:06 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+
+import streamlit as st
+import openai
+import json
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key #secretsからAPI keyを取得する。
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini-2024-07-18",
+        messages=messages
+    )
+
+    bot_message = response.model_dump_json(indent=2)
+    bot_message = json.loads(bot_message)
+    bot_message = bot_message["choices"][0]["message"]
+    #GPTからのメッセージ、このメッセージもroleとcontentの形式になっている。
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title("My AI Assistant")
+st.write("ChatGPT APIを使ったチャットボットです。")
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "🙂"
+        if message["role"]=="assistant":
+            speaker="🤖"
+
+        st.write(speaker + ": " + message["content"])
